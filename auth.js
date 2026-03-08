ec349b1e42ca95b43ad6daf50132c4868438509d
Solomon1011 <168952928+Solomon1011@users.noreply.github.com>
2026-03-08 15:03:42 +0100

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,41 @@
+import { signup, login } from "./app.js";
+
+const form = document.getElementById("authForm");
+
+if (form) {
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const action = form.dataset.action;
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (action === "signup") {
+
+      const name = document.getElementById("name").value;
+
+      const user = await signup(email, password, name);
+
+      if (user) {
+        alert("Signup successful!");
+        window.location.href = "profile_setup.html";
+      }
+
+    }
+
+    if (action === "login") {
+
+      const user = await login(email, password);
+
+      if (user) {
+        alert("Login successful!");
+        window.location.href = "dashboard.html";
+      }
+
+    }
+
+  });
+
+}
